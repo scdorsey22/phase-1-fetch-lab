190d8965cbbaa564ff18fcefef8377d5d5004604
Shane Dorsey <scdorsey22@gmail.com>
2022-05-25 09:00:35 -0700

    commit tree 82180f82a96709b2eaddc68b97e9ac5d9b05936b

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 function fetchBooks() {
   // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+    .then((response) => response.json())
+    .then((json) => renderBooks(json))
+
+    
+
+    
 }
 
 function renderBooks(books) {
@@ -15,3 +21,4 @@ function renderBooks(books) {
 document.addEventListener('DOMContentLoaded', function() {
   fetchBooks();
 });
+
